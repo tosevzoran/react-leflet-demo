81f0e3278993223990f67664df8b0333458ba6da
Zoran Tošev <zoran.tosev@symphony.is>
2022-11-28 19:02:01 +0100

    chore(*): add a minimal PWA resources

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,34 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: ["vite.svg", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        name: "Leaflet demo",
+        short_name: "LeafletDemo",
+        description: "A leaflet and react demo",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          // {
+          //   src: "pwa-512.png",
+          //   sizes: "512x512",
+          //   type: "image/png",
+          //   purpose: "any maskable",
+          // },
+        ],
+      },
     }),
     react(),
   ],
